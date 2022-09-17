cdd5f897f1bc502a7cc2e2446b47cb8fdc8d4b09
wprabhakar <kwchess@gmail.com>
2022-09-17 13:15:38 +0000

    BLU-001 2022-09-17 13:15

---
+++ 6666.0503dcbc84910439.js
@@ -0,0 +1,4 @@
+"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[6666],{6666:(P,n,t)=>{t.r(n),t.d(n,{startStatusTap:()=>i});var a=t(5861),o=t(7205),_=t(8935),r=t(3756);
+/*!
+     * (C) Ionic http://ionicframework.com - MIT License
+     */const i=()=>{const s=window;s.addEventListener("statusTap",()=>{(0,o.f)(()=>{const d=document.elementFromPoint(s.innerWidth/2,s.innerHeight/2);if(!d)return;const e=(0,_.f)(d);e&&new Promise(h=>(0,r.c)(e,h)).then(()=>{(0,o.c)((0,a.Z)(function*(){e.style.setProperty("--overflow","hidden"),yield(0,_.s)(e,300),e.style.removeProperty("--overflow")}))})})})}}}]);
